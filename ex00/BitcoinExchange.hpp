a15243697229cc8d7b49d77d5a4be53d0c33b53b
jocologne <pjz_2000@PC.localdomain>
2025-11-27 01:04:12 -0300

    ex00

--- ex00/BitcoinExchange.hpp
+++ ex00/BitcoinExchange.hpp
@@ -11,18 +11,18 @@
 
 class BitcoinExchange {
 private:
-    std::map<std::string, float> _exchangeRates;
-    bool isValidDate(const std::string &date) const;
-    bool isValidValue(const std::string &value) const;
-    float getRate(const std::string &date) const;
+	std::map<std::string, float> _exchangeRates;
+	bool isValidDate(const std::string &date) const;
+	bool isValidValue(const std::string &value) const;
+	float getRate(const std::string &date) const;
 public:
-    BitcoinExchange();
-    ~BitcoinExchange();
-    BitcoinExchange(const BitcoinExchange &other);
-    BitcoinExchange &operator=(const BitcoinExchange &other);
+	BitcoinExchange();
+	~BitcoinExchange();
+	BitcoinExchange(const BitcoinExchange &other);
+	BitcoinExchange &operator=(const BitcoinExchange &other);
 
-    void loadExchangeRates(const std::string &filename);
-    void processInput(const std::string &filename) const;
+	void loadExchangeRates(const std::string &filename);
+	void processInput(const std::string &filename) const;
 };
 
 #endif	
